397c332f902dbb6571c11445c8791eef89e00e6e
Jan Brombach <jan.brombach@besonders-sein.de>
2018-04-05 12:39:23 +0200

    Changed Title

--- header.php
+++ header.php
@@ -15,6 +15,7 @@ if (!isset($_SESSION['post-data'])){
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <title></title>
+  <title>Neuer Titel!</title>
   
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/main.css">
